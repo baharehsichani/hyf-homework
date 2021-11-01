25fe1cebf7adebd077632b9f42081d7465a07f9d
ba1979102 <bahshc@gmail.com>
2021-11-01 20:47:08 +0100

    fixed index2.js

--- javascript/javascript1/week1/JS-Homework/index2.js
+++ javascript/javascript1/week1/JS-Homework/index2.js
@@ -6,7 +6,7 @@ const dogYear = dogYearFuture - dogYearOfBirth ;
 const shouldShowResultInDogYears=false; 
 
 if (shouldShowResultInDogYears==true){
-    console.log("Your dog will be " + (dogYear*7) + " dog years old in " + dogYearFuture);
+    console.log("Your dog will be " + (dogYear*=7) + " dog years old in " + dogYearFuture);
 }else{
     console.log("Your dog will be " + dogYear + " human years old in " + dogYearFuture);
-}
+}git branch
