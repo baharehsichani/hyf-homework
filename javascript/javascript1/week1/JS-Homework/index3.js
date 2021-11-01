736449a89b8c07200afbec68b5a0f993d7388453
ba1979102 <bahshc@gmail.com>
2021-11-01 21:00:12 +0100

    fixed index3.js

--- javascript/javascript1/week1/JS-Homework/index3.js
+++ javascript/javascript1/week1/JS-Homework/index3.js
@@ -1,34 +1,36 @@
 console.log("A house price estimator");
 // Peter 
-let wide=8;
-let deep=10;
-let high=10;
+let houseWidth=8;
+let houseDeep=10;
+let houseHeight=10;
 
-let volumeInMeters= wide*high*deep;
+let volumeInMeters= houseWidth*houseDeep*houseHeight;
 let gardenSizeInM2=100;
 let houseCosts= 2500000;
 
-let housePrice = volumeInMeters * 2.5 * 1000 + gardenSizeInM2 * 300;
+let housePrice =Math.abs(volumeInMeters * 2.5 * 1000 + gardenSizeInM2 * 300);
 
 if (housePrice>houseCosts){
     console.log("Peter is paying too much");
-}else{
+}else if(housePrice<houseCosts){
     console.log("Peter is paying too little");
-}
+}else
+console.log("Julia is paying just the right amount")
 
 // Julia  
-wide=5;
-deep=11;
-high=8;
+houseWidth=5;
+houseDeep=11;
+houseHeight=8;
 
-volumeInMeters= wide*high*deep;
+volumeInMeters= houseWidth*houseDeep*houseHeight;
 gardenSizeInM2=70;
 houseCosts= 1000000;
 
-housePrice = volumeInMeters * 2.5 * 1000 + gardenSizeInM2 * 300;
+housePrice =Math.abs(volumeInMeters * 2.5 * 1000 + gardenSizeInM2 * 300);
 
 if (housePrice>houseCosts){
     console.log("Julia is paying too much");
-}else{
+}else if(housePrice<houseCosts){
     console.log("Julia is paying too little");
-}
+}else
+console.log("Julia is paying just the right amount")
